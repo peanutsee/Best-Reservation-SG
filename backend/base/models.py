5cb0745cd8d82fb257bb6c14364e86472f2bf220
Parcelvy-Admin <dsee007@yahoo.com>
2022-02-22 15:29:54 +0800

    config and tested login and registration backend endpoints

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# ----- USER MODELS -----
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    contact_number = models.CharField(max_length=255)
+
+    objects = models.manager
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+# ----- RESERVATION MODELS ------
+
+# ----- PAYMENT MODELS ------
+
+# ----- BILL MODELS -----
+
+# ----- PRE-ORDER MODELS -----
--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from base.views import user_views as views
+
+"""
+Handles all URL paths related to users.
+"""
 
 urlpatterns = [
+    # Authentication
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', views.registerUser, name='register'),
 ]
 
 
