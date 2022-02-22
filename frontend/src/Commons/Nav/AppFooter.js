7a769de5de2f420718a7bfe5a0fa321701a04ffe
Parcelvy-Admin <dsee007@yahoo.com>
2022-02-23 01:03:13 +0800

    added files for header and footer

---
+++ frontend/src/Commons/Nav/AppFooter.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+function AppFooter() {
+  return (
+    <div>AppFooter</div>
+  );
+}
+
+export default AppFooter;
---
+++ frontend/src/Commons/Nav/AppHeader.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+function AppHeader() {
+  return (
+    <div>AppHeader</div>
+  );
+}
+
+export default AppHeader;
