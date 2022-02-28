7c55af08593301372ae81b9211f530a57175e7f8
Take-Saori <71936501+Take-Saori@users.noreply.github.com>
2022-02-28 15:18:05 +0800

    implemented Footer UI

--- frontend/src/Commons/Nav/AppFooter.js
+++ frontend/src/Commons/Nav/AppFooter.js
@@ -1,10 +1,77 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable linebreak-style */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  ListGroup,
+  Button,
+} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function AppFooter() {
   return (
-    <div>AppFooter</div>
+    <footer className="footer-clean bg-light py-5">
+      <Container>
+        <div className="d-flex justify-content-center align-center">
+          <Button onClick={() => window.scrollTo(0, 0)}>
+            <i className="fas fa-chevron-up" />
+            <div>Scroll to Top</div>
+          </Button>
+        </div>
+        <Row className="justify-content-center">
+          <Col sm={12} md={6} item className="my-3 borderless text-center">
+            <h4>Quick Links</h4>
+            <ListGroup as="ul">
+              <ListGroup.Item>
+                <div>
+                  <Link to="/aboutus">About Us</Link>
+                </div>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div>
+                  <Link to="/faq">FAQ</Link>
+                </div>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div>
+                  <Link to="/donate">Donate</Link>
+                </div>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div>
+                  <Link to="/contactus">Contact Us</Link>
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col sm={12} md={6} item className="my-3 borderless text-center">
+            <h4>Important Links</h4>
+            <ListGroup as="ul">
+              <ListGroup.Item>
+                <div>
+                  <Link to="/termsandcondition">Terms & Conditions</Link>
+                </div>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div>
+                  <Link to="/privacypolicy">Privacy Policy</Link>
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+
+        <Row className="mt-4">
+          <div className="text-center pt-2">
+            <p className="copyright">VolunteerBroz © 2021</p>
+          </div>
+        </Row>
+      </Container>
+    </footer>
   );
 }
 
