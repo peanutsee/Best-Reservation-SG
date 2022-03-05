fd6c301129fec930a6c939920f370f13c2ca5afb
KaiYun99 <laukaiyun1999@gmail.com>
2022-03-05 16:02:04 +0800

    json conflict

--- backend/base/urls/reservation_urls.py
+++ backend/base/urls/reservation_urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('get-all-reservation/', views.getAllReservation, name='get-all-reservation'),
     path('get-all-completed-reservation/', views.getAllCompletedReservation, name='get-all-completed-reservation'),
     path('get-all-active-reservation/', views.getAllActiveReservation, name='get-all-active-reservation'),
-    path('get-reservation/reservation_id=<int:pk>', views.getReservation, name='get-reservation'),
-    path('update-reservation/reservation_id=<int:pk>/', views.updateReservation, name='update-reservation'),
-    path('delete-reservation/reservation_id=<int:pk>', views.deleteReservation, name='delete-reservation'),
-    path('join-reservation/reservation_id=<int:pk>', views.joinReservation, name='join-reservation'),
-    path('remove-reservation/reservation_id=<int:pk>', views.removeReservation, name='remove-reservation'),
+    path('get-reservation/<int:pk>', views.getReservation, name='get-reservation'),
+    path('update-reservation/<int:pk>/', views.updateReservation, name='update-reservation'),
+    path('delete-reservation/<int:pk>', views.deleteReservation, name='delete-reservation'),
+
 ]
+
+
