190566d46e09da7e4263caf5890ef19533c7b293
Parcelvy-Admin <dsee007@yahoo.com>
2022-03-17 00:59:26 +0800

    set up to load restaurant data

--- backend/base/views/restaurant_views.py
+++ backend/base/views/restaurant_views.py
@@ -3,21 +3,48 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..serializers import *
 from random import shuffle
-
-# TODO: Include "Search" and "Filter" functions
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+import json
 
 @api_view(['GET'])
 def retrieveAllRestaurants(request):
     """
     Retrieve Restaurant with No Filter
     """
+    # RUN ONCE TO LOAD RESTAURANTS INTO DATABASE
+    file_paths = [r'C:\Users\Darryl See\Desktop\cz2006-project\backend\base\views\formatted_clean_chinese_food.json',r'C:\Users\Darryl See\Desktop\cz2006-project\backend\base\views\formatted_clean_muslim_food.json',r'C:\Users\Darryl See\Desktop\cz2006-project\backend\base\views\formatted_clean_indian_food.json',r'C:\Users\Darryl See\Desktop\cz2006-project\backend\base\views\formatted_clean_japanese_food.json',r'C:\Users\Darryl See\Desktop\cz2006-project\backend\base\views\formatted_clean_western_food.json' ]
+    for f in file_paths:
+        with open(f, encoding='utf-8') as file:
+            f = json.load(file)
+            # TODO: Load into database
+    
     restaurants = Restaurant.objects.all()
     restaurants_serialized = RestaurantSerializer(restaurants, many=True)
     restaurants_data = restaurants_serialized.data
     shuffle(restaurants_data)
     
-    # TODO: Pagination
-    return Response(restaurants_data, status=status.HTTP_200_OK)
+    # Restaurant Pagination TODO: TEST PAGINATOR
+    paginator = Paginator(restaurants_data, 12)
+    page = request.query_params.get('page')
+    try:
+        restaurants_data = paginator.page(page)
+    except PageNotAnInteger:
+        restaurants_data = paginator.page(1)
+    except EmptyPage:
+        restaurants_data = paginator.page(paginator.num_pages)
+
+    if page == None:
+        page = 1
+
+    page = int(page)
+    
+    output = {
+        'restaurants_data': restaurants_data,
+        'page': page,
+        'pages': paginator.num_pages
+    }
+
+    return Response(output, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def retrieveAllRestaurantsByMostClicks(request):
@@ -27,9 +54,28 @@ def retrieveAllRestaurantsByMostClicks(request):
     restaurants = Restaurant.objects.all().order_by('-num_clicks')
     restaurants_serialized = RestaurantSerializer(restaurants, many=True)
     restaurants_data = restaurants_serialized.data
-    shuffle(restaurants_data)
-    return Response(restaurants_data, status=status.HTTP_200_OK)
+    
+    # Restaurant Pagination TODO: TEST PAGINATOR
+    paginator = Paginator(restaurants_data, 12)
+    page = request.query_params.get('page')
+    try:
+        restaurants_data = paginator.page(page)
+    except PageNotAnInteger:
+        restaurants_data = paginator.page(1)
+    except EmptyPage:
+        restaurants_data = paginator.page(paginator.num_pages)
 
+    if page == None:
+        page = 1
+
+    page = int(page)
+    
+    output = {
+        'restaurants_data': restaurants_data,
+        'page': page,
+        'pages': paginator.num_pages
+    }
+    return Response(output, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
