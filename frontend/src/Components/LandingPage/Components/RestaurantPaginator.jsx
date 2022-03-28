5fd7d1713abfb5f1b687f14a07330ed90c9a03ee
Parcelvy-Admin <dsee007@yahoo.com>
2022-03-28 17:14:05 +0800

    fixed paginator for many items

--- frontend/src/Components/LandingPage/Components/RestaurantPaginator.jsx
+++ frontend/src/Components/LandingPage/Components/RestaurantPaginator.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
@@ -10,7 +10,6 @@ function RestaurantPaginator({ pages, page }) {
         {[...Array(pages).keys()].map((x) => (
           <LinkContainer key={x + 1} to={`/?page=${x + 1}`}>
             <Pagination.Item
-              className="border border-primary"
               active={x + 1 === page}
             >
               {x + 1}
@@ -20,8 +19,46 @@ function RestaurantPaginator({ pages, page }) {
       </Pagination>
     );
   }
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const createPaginationItem = (x) => (
+    <LinkContainer key={x} to={`/?page=${x}`}>
+      <Pagination.Item
+        active={x === page}
+      >
+        {x}
+      </Pagination.Item>
+    </LinkContainer>
+  );
+
+  const paginationItems = [];
+  // Add the first item (page 1)
+  paginationItems.push(createPaginationItem(1));
+  // Add an ellipsis
+  paginationItems.push(<Pagination.Ellipsis />);
+  const midpoint = pages / 2;
+  // Create page numbers in the middle
+  for (let i = midpoint; i <= midpoint + 4; i += 1) {
+    paginationItems.push(createPaginationItem(i));
+  }
+  // Add an ellipsis
+  paginationItems.push(<Pagination.Ellipsis />);
+  // Add the last item (page N)
+  paginationItems.push(createPaginationItem(pages));
+
   return (
-    <p>Fix the Paginator</p>
+    <Pagination className="paginationInfo">
+      <Pagination.Prev
+        onClick={() => setCurrentPage(currentPage - 1)}
+        disabled={currentPage === 1}
+      />
+      {paginationItems}
+      <Pagination.Next
+        onClick={() => setCurrentPage(currentPage + 1)}
+        disabled={currentPage === pages}
+      />
+    </Pagination>
   );
 }
 export default RestaurantPaginator;
