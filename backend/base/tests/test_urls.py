36ec74b76b36a4505fa5da8b14b194592fabfd35
Parcelvy-Admin <dsee007@yahoo.com>
2022-03-31 13:59:40 +0800

    refactor backend, set up test env for backend unit testing

---
+++ backend/base/tests/test_urls.py
@@ -0,0 +1,6 @@
+from django.test import SimpleTestCase
+
+class TestUrls(SimpleTestCase):
+
+    def test_list_url_is_resolve(self):
+        assert 1 == 2
--- backend/base/urls/pre_order_urls.py
+++ backend/base/urls/pre_order_urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from ..views import pre_order_views as views
 
 urlpatterns = [
-    path('list-order-items/order_id=<int:order_pk>/', views.retrieveAllOrders),
-    path('add-item-to-order/order_id=<int:order_pk>/item_id=<int:item_id>/', views.addItemToOrder),
-    path('update-order-items-qty/item_order_key=<int:item_order_key>/', views.updateItemInOrder),
-    path('delete-order-items/item_order_key=<int:item_order_key>/', views.removeItemInOrder),
-    path('retrieve-menu-items/restaurant_id=<int:restaurant_id>/', views.retrieveMenu),
+    path('list-order-items/order_id=<int:order_pk>/', views.retrieveAllOrders, name='retrieve-all-orders'),
+    path('add-item-to-order/order_id=<int:order_pk>/item_id=<int:item_id>/', views.addItemToOrder, name='add-item-orders'),
+    path('update-order-items-qty/item_order_key=<int:item_order_key>/', views.updateItemInOrder, name='update-item-orders'),
+    path('delete-order-items/item_order_key=<int:item_order_key>/', views.removeItemInOrder, name='remove-item-orders'),
+    path('retrieve-menu-items/restaurant_id=<int:restaurant_id>/', views.retrieveMenu, name='retrieve-menu'),
 ]
 
 
