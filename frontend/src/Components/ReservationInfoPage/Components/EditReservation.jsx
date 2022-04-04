ae7341937f35adf22fc28da964737b0a8779f1ed
KaiYun99 <laukaiyun1999@gmail.com>
2022-04-04 14:33:05 +0800

    Implemented edit reservation details

--- frontend/src/Components/ReservationInfoPage/Components/EditReservation.jsx
+++ frontend/src/Components/ReservationInfoPage/Components/EditReservation.jsx
@@ -1,12 +1,41 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import { React } from 'react';
 import {
   Modal, Button, Form,
 } from 'react-bootstrap';
-import DateForm from './DateForm';
-import TimeForm from './TimeForm';
+import { useDispatch } from 'react-redux';
+import { updateReservation } from '../Redux/actions';
 
 function EditPopUpModal(props) {
+  const { setEditModalShow, reservation, setEdited } = props;
+  const dispatch = useDispatch();
+  let reservationDate = reservation.reservation_date;
+  let reservationTime = reservation.reservation_time;
+  let reservationPax = reservation.reservation_pax;
+  let reservationDateTime = `${reservationDate} ${reservationTime}`;
+
+  const handleDateChange = (e) => {
+    reservationDate = e;
+    reservationDateTime = `${reservationDate} ${reservationTime}`;
+  };
+
+  const handleTimeChange = (e) => {
+    reservationTime = e;
+    reservationDateTime = `${reservationDate} ${reservationTime}`;
+  };
+
+  const handlePaxChange = (e) => {
+    reservationPax = e;
+  };
+
+  const handleSubmit = () => {
+    dispatch(updateReservation(reservation.id, reservationDateTime, reservationPax));
+    setEditModalShow(false);
+    setEdited(true);
+  };
+
   return (
     <Modal
       {...props}
@@ -21,20 +50,33 @@ function EditPopUpModal(props) {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Label>Reservation Date</Form.Label>
+          <Form.Label>
+            Reservation Date
+          </Form.Label>
           <Form.Group>
-            <DateForm />
+            <Form.Control
+              type="date"
+              onChange={(e) => handleDateChange(e.target.value)}
+            />
+            {' '}
+
           </Form.Group>
         </Form>
         <Form>
-          <Form.Label>Reservation Time</Form.Label>
+          <Form.Label>
+            Reservation Time
+
+          </Form.Label>
           <Form.Group>
-            <TimeForm />
+            <Form.Control
+              type="time"
+              onChange={(e) => handleTimeChange(e.target.value)}
+            />
           </Form.Group>
         </Form>
         <Form.Group>
           <Form.Label>Number of Guests</Form.Label>
-          <Form.Select>
+          <Form.Select onChange={(e) => handlePaxChange(e.target.value)}>
             <option>1</option>
             <option>2</option>
             <option>3</option>
@@ -44,7 +86,7 @@ function EditPopUpModal(props) {
         </Form.Group>
       </Modal.Body>
       <Modal.Footer>
-        <Button>Confirm</Button>
+        <Button onClick={() => handleSubmit()}>Confirm</Button>
       </Modal.Footer>
     </Modal>
   );
